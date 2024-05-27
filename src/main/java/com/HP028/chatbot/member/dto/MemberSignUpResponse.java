6cb11edb03260f79b20bc49eddec4d48479bb435
sanikani <sanikani@naver.com>
2024-05-27 23:32:13 +0900

    [Feat] 회원가입, 로그인 DTO 수정

--- src/main/java/com/HP028/chatbot/member/dto/MemberSignUpResponse.java
+++ src/main/java/com/HP028/chatbot/member/dto/MemberSignUpResponse.java
@@ -1,9 +1,5 @@
 package com.HP028.chatbot.member.dto;
 
-import com.HP028.chatbot.member.domain.Member;
-import lombok.Getter;
-
-@Getter
 public class MemberSignUpResponse {
 
     private String email;
