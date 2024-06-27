8ec34d4652d7604919a8c5967ab531fad3547f35
sanikani <sanikani@naver.com>
2024-06-27 22:14:20 +0900

    환경변수 사용 수정

---
+++ src/main/java/com/HP028/chatbot/chat/controller/ChatController.java
@@ -0,0 +1,17 @@
+package com.HP028.chatbot.chat.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/chat")
+public class ChatController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
