14b64a1ca6b9322f7dff1a3d4c4dac906497b2f0
sanikani <sanikani@naver.com>
2024-08-29 17:24:18 +0900

    [Feat] : 채팅 메시지 검증
    메시지 길이 검증

--- src/main/java/com/HP028/chatbot/chat/controller/ChatMessageController.java
+++ src/main/java/com/HP028/chatbot/chat/controller/ChatMessageController.java
@@ -6,6 +6,7 @@ import com.HP028.chatbot.chat.dto.SendChatMessageResponse;
 import com.HP028.chatbot.chat.service.ChatMessageService;
 import com.HP028.chatbot.common.response.ApiResponse;
 import com.HP028.chatbot.common.response.ApiSuccessStatus;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +21,7 @@ public class ChatMessageController {
     private final ChatMessageService chatMessageService;
 
     @PostMapping
-    public ResponseEntity<ApiResponse<SendChatMessageResponse>> sendAndReceiveMessage(@RequestBody SendChatMessageRequest request) {
+    public ResponseEntity<ApiResponse<SendChatMessageResponse>> sendAndReceiveMessage(@RequestBody @Valid SendChatMessageRequest request) {
         return ApiResponse.success(ApiSuccessStatus.MESSAGE_SEND_AND_RECEIVE_SUCCESS,chatMessageService.sendMessage(request));
     }
 
